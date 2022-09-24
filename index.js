2f7af7fea57ff4c624cf99b48762990655260be0
NNTKOALA <trungnngch200639@fpt.edu.vn>
2022-09-24 17:29:23 +0700

    hbs

--- index.js
+++ index.js
@@ -16,18 +16,22 @@ app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-    var name = "Cloud Computing";
-    var code = 1644;
-    var semeter = "Fall 2022";
-    res.render('index', {name: name, code: code, semeter: semeter});
-})
-
-app.get("/mobile", (req, res) => {
-    res.render("mobile");
+    let name = "Cloud Computing"; //string
+    let code = 1644; //int
+    //pass dữ liệu từ back-end sang front-end
+    //cú pháp: name (data) : value (data)
+    //gửi dữ liệu là value và nhận với tên là name
+    res.render("index", { name: name, ma: code, ky: "Fall 2022" });
   });
   
-app.get("/laptop", (req, res) => {
-    res.render("laptop");
+  app.get("/mobile", (req, res) => {
+    let list = ["iPhone", "Samsung", "Oppo", "Huawei"];
+    res.render("mobile", { mobiles: list });
+  });
+  
+  app.get("/laptop", (req, res) => {
+    let list = ["Macbook", "Dell", "HP", "Asus", "LG"];
+    res.render("laptop", { laptops: list });
   });
 
 //listen port để chạy server
